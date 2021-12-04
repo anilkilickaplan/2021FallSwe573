aaafc118139997268dd8e65938210fcd53b6a0bc
Anil Kilickaplan <70136838+anilkilickaplan@users.noreply.github.com>
2021-12-05 00:24:45 +0300

    Docker files corrected with Corks packages

--- ShareClub/settings.py
+++ ShareClub/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('DJANGO_SECRET_KEY')
+SECRET_KEY = env('DJANGO_SECRET_KEY', default = 'm!rockf+snbas+3$6iwo#54&x@&@rw7x=d6c0t4au4gunb9&y4')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DJANGO_DEBUG', default=False)
+DEBUG = env('DJANGO_DEBUG', default=True)
 
 ALLOWED_HOSTS = []
 
